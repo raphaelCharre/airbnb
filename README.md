ae082710cbf0a22052c40970b5b3d6620706ea77
Raphaël Charre <rcharre@oceaneconsulting.com>
2018-10-11 17:01:36 +0200

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # airbnb
+
+A lancer avec un serveur web
+index.html se trouve dans /html -> on repassera pour l'archi.
---
+++ js/constants.js
@@ -0,0 +1,35 @@
+const announces = [
+    {
+        type: 'Logement entier',
+        title : 'Appartement spa hypercentre nantes',
+        image_path : '../img/annonce1.png',
+        description : '2 voyageurs, 1 chambre, 1 salle de bain, Wi-fi, cuisine.',
+        price : 65,
+        price_unite : 'par nuit',
+        mark : 4.5,
+        mark_number : 311,
+        cancel_price : 0
+    },
+    {
+        type: 'Logement entier',
+        title : 'Charmant appartement Vue exceptionnelle',
+        image_path : '../img/annonce2.png',
+        description : '3 voyageurs, 1 chambre, 2 lits, 1 salle de bain.',
+        price : 53,
+        price_unite : 'par nuit',
+        mark : 4.5,
+        mark_number : 346,
+        cancel_price : 0
+    },
+    {
+        type: 'Logement entier',
+        title : 'Charmant studio 2 pieces',
+        image_path : '../img/annonce3.png',
+        description : '2 voyageurs, 1 chambre, 1 lits, 1 salle de bain.',
+        price : 45,
+        price_unite : 'par nuit',
+        mark : 5,
+        mark_number : 256,
+        cancel_price : 0
+    }
+]
